772f43a4547b71b7026d2b0c6b07ea0a027c2821
Victor Badaró <victor.badaro@fatec.sp.gov.br>
2023-12-04 15:47:06 -0300

    feat: add socket.io and some new configs

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,16 @@
 import express from 'express';
+import { createServer } from 'node:http';
+import path from 'node:path';
+import { Server } from 'socket.io';
 
 const app = express();
+const server = createServer(app);
+const io = new Server(server);
 
-app.listen(3000, () => console.log('Server is now running on port 3000'));
+app.use(express.static(path.join(__dirname, '..', 'public')));
+
+io.on('connection', (socket) => {
+  console.log('Socket', socket.id);
+});
+
+server.listen(3000, () => console.log('Server is now running on port 3000'));
